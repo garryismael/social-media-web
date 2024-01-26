852bc7b174eadfa9c2b989c000464b3dc40e1074
Garry <tahinjanaharygarry@gmail.com>
2024-01-26 22:34:03 +0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,65 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { SavedPostComponent } from './pages/saved-post/saved-post.component';
+import { PostComponent } from './pages/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    redirectTo: 'auth/sign-up',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'explore',
+        component: ExploreComponent,
+      },
+      {
+        path: 'all-users',
+        component: PeopleComponent,
+      },
+      {
+        path: 'saved',
+        component: SavedPostComponent,
+      },
+      {
+        path: 'create-post',
+        component: PostComponent,
+      },
+    ],
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'sign-in',
+        component: SignInComponent,
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent,
+      },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
