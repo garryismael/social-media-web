65733494801d6b6a936827be1c46daa500d4fa33
Garry <tahinjanaharygarry@gmail.com>
2024-01-26 22:38:21 +0300

    create saved post page

---
+++ src/app/pages/saved-post/saved-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './saved-post.component.html',
+  styleUrl: './saved-post.component.css'
+})
+export class SavedPostComponent {
+
+}
